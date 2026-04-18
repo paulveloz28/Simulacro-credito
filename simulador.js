594c489dae9ac995df575588283356e0e2bfe3f8
paulveloz28 <paulveloz28@gmail.com>
2026-04-18 18:51:46 -0500

    feat: add function calcular

--- simulador.js
+++ simulador.js
@@ -1,2 +1,20 @@
 //AQUI EL JAVASCRIPT PARA MANIPULAR EL HTML
+function calcular(){
+    let ingresosFloat=0;
+    let egresosFloat=0;
+    let cmpIngresosFloat;
+    let cmpEgresosFloat;
+    let total;
+    let saldoDisponible;
+    let capacidadDePago;
+    let mostrarCapacidadPago
 
+    cmpIngresosFloat=document.getElementById("txtIngresos");
+    cmpEgresosFloat=document.getElementById("txtEgresos");
+    ingresosFloat=parseFloat(cmpIngresosFloat.value);
+    egresosFloat=parseFloat(cmpEgresosFloat.value);
+
+    saldoDisponible=calcularDisponible(ingresosFloat,egresosFloat);
+    total=document.getElementById("spnDisponible");
+    total.innerText=saldoDisponible;
+}
